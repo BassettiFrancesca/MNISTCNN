d47fe3368f71245f57f994d0206c7a10b8301b16
Francesca <bassetti.fra@gmail.com>
2021-01-19 19:24:07 +0100

    Added timer

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import testing
 import training
+import time
 
 
 def mnist_cnn():
 
+    start = time.time()
+
     training.train()
     testing.test()
+
+    finish = time.time()
+
+    print('Seconds passed : %f' % (finish - start))
 
 
 if __name__ == '__main__':
--- training.py
+++ training.py
@@ -8,8 +8,7 @@ import torch.nn as nn
 
 def train():
 
-    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cuda')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
